418229903417a991829699f5bfda4cb5c40a77fc
Shreyans Dongare <69852059+shreydm@users.noreply.github.com>
2024-01-27 14:43:09 +0530

    Update ac1.py

--- ac1.py
+++ ac1.py
@@ -7,5 +7,5 @@ reader = pd.read_csv("data.csv")
 si = reader.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
 print(si)
 
-graph = pg.Scatter(si, x = 'student_id', y = 'level', size = 'attempt', color = 'attempt')
+graph = pg.scatter(si, x = 'student_id', y = 'level', size = 'attempt', color = 'attempt')
 graph.show()
